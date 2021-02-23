f9a20cb01ae1b0400e1ab03e527a465ffee3fc51
hugh-zhan9 <hugh_zhan9@163.com>
2021-02-23 23:23:34 +0800

    商品种类树形结构展示

--- mallonline-product/src/main/java/com/hugh/mallonline/product/MallonlineProductApplication.java
+++ mallonline-product/src/main/java/com/hugh/mallonline/product/MallonlineProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 @EnableFeignClients(basePackages = "com.hugh.mallonline.product.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -15,5 +16,4 @@ public class MallonlineProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallonlineProductApplication.class, args);
     }
-
 }
--- mallonline-product/src/main/java/com/hugh/mallonline/product/controller/CategoryController.java
+++ mallonline-product/src/main/java/com/hugh/mallonline/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.hugh.mallonline.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -32,14 +33,14 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 以树形结构组装
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("data", entities);
     }
 
 
